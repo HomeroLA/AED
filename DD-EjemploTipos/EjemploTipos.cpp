6127b8aef8cc842b60f5da4b3d7b7064f42686a1
HomeroLA <70210818+HomeroLA@users.noreply.github.com>
2021-05-06 20:23:07 -0300

    Add files via upload

--- DD-EjemploTipos/EjemploTipos.cpp
+++ DD-EjemploTipos/EjemploTipos.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 int main(){
 	
-	assert(-2==-2); //dato entero
+	assert(-2-2==-4); //dato entero
 	assert("abc"=="abc"); //dato string
 	assert(3.54==3.54); //dato double
 	assert("b"=="b"); //dato char
 	assert(true==true); //dato bool
-	assert(3==3); //dato unsigned 
+	assert(3+1==4); //dato unsigned 
 	
 	return 0;
 }
