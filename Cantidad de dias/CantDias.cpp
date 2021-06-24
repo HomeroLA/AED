259bc350c07fc81923a7388112398ee0449f8d1e
HomeroLA <70210818+HomeroLA@users.noreply.github.com>
2021-06-24 16:55:34 -0300

    Update CantDias.cpp

--- Cantidad de dias/CantDias.cpp
+++ Cantidad de dias/CantDias.cpp
@@ -1,40 +1,59 @@
-#include<iostream>
+#include <iostream>
 #include<cassert>
 
 using namespace std;
 
-int cantDias(int,int);
-int EsBisiesto (int);
+int cantDias(int, int);
 
-int main(){
-	
-	int m, a;
+int diasFebrero(int);
 
-	cout<<"Ingrese mes: "<<endl;
-	cin>>m;
-	m>0 && m<13;
-	cout<<"Ingrese anio: "<<endl;
-	cin>>a;
-		a>1582;
-	
-	cout<<cantDias(m,a);
-	assert(29==cantDias(2,2016));
-	assert(30==cantDias(4,2021)); 
-	assert(29==cantDias(2,1988));
-	assert(31==cantDias(12,1950)); 
-	assert(30==cantDias(6,2021));
+bool esMesValido(int);
+
+bool esAnioValido(int);
+
+bool mensajeDatoInvalido();
+
+int main() {
+    int m, a;
+    bool valid = false;
+
+    cout << "Ingrese mes: " << endl;
+    cin >> m;
+    cout << "Ingrese anio: " << endl;
+    cin >> a;
+
+    esMesValido(m) && esAnioValido(a) ? valid = true : mensajeDatoInvalido();
+
+    valid ? cout << "La cantidad de dias es: " : cout << "" << endl;
+    valid ? cout << cantDias(m, a) : cout << "No es posible calcular la cantidad de dias con los valores ingresados.";
+
+    assert(29 == cantDias(2, 2016));
+    assert(30 == cantDias(4, 2021));
+    assert(29 == cantDias(2, 1988));
+    assert(31 == cantDias(12, 1950));
+    assert(30 == cantDias(6, 2021));
+    assert(28 == cantDias(2, 2003));
 }
 
-int cantDias(int m, int a ){
-	a>1582;
-	m>0 && m<13;
-return 
-	(m== 4) ||(m==6)  || (m==9)|| (m==11) ? 30      :
-	(m== 1 )|| (m==3) || (m==5)|| (m==7) || (m==8) || (m==10) || (m==12) ? 31 :
-		EsBisiesto(a);
+int cantDias(int m, int a) {
+    return (m == 4) || (m == 6) || (m == 9) || (m == 11) ? 30 :
+           (m == 1) || (m == 3) || (m == 5) || (m == 7) || (m == 8) || (m == 10) || (m == 12) ? 31 :
+           diasFebrero(a);
 }
 
-int EsBisiesto (int anio){
-return anio%4==0||anio%100==0 && anio%400==0 ? 29:
-	28;
+int diasFebrero(int anio) {
+    return anio % 4 == 0 && (anio % 100 != 0 || anio % 400 == 0) ? 29 : 28;
+}
+
+bool esMesValido(int mes) {
+    return mes > 0 && mes < 13;
+}
+
+bool esAnioValido(int anio) {
+    return anio > 1582;
+}
+
+bool mensajeDatoInvalido() {
+    cout << "Algun dato ingresado no es valido";
+    return false;
 }
