08a95cefe800441e4eaec15c131e34b41d54e39e
HomeroLA <70210818+HomeroLA@users.noreply.github.com>
2021-06-24 19:03:17 -0300

    Update CantDias.cpp

--- Cantidad de dias/CantDias.cpp
+++ Cantidad de dias/CantDias.cpp
@@ -3,30 +3,27 @@
 
 using namespace std;
 
+
 int cantDias(int, int);
-
 int diasFebrero(int);
-
 bool esMesValido(int);
-
 bool esAnioValido(int);
-
 bool mensajeDatoInvalido();
 
 int main() {
-    int m, a;
+    int mes, anio;
+
     bool valid = false;
 
     cout << "Ingrese mes: " << endl;
-    cin >> m;
+    cin >> mes;
     cout << "Ingrese anio: " << endl;
-    cin >> a;
+    cin >> anio;
 
-    esMesValido(m) && esAnioValido(a) ? valid = true : mensajeDatoInvalido();
+    esMesValido(mes) && esAnioValido(anio) ? valid = true : mensajeDatoInvalido(); 
 
-    valid ? cout << "La cantidad de dias es: " : cout << "" << endl;
-    valid ? cout << cantDias(m, a) : cout << "No es posible calcular la cantidad de dias con los valores ingresados.";
-
+    valid ? cout << "La cantidad de dias es: " << cantDias(mes, anio) : cout << "No es posible calcular la cantidad de dias con los valores ingresados.";
+    
     assert(29 == cantDias(2, 2016));
     assert(30 == cantDias(4, 2021));
     assert(29 == cantDias(2, 1988));
@@ -35,14 +32,13 @@ int main() {
     assert(28 == cantDias(2, 2003));
 }
 
-int cantDias(int m, int a) {
-    return (m == 4) || (m == 6) || (m == 9) || (m == 11) ? 30 :
-           (m == 1) || (m == 3) || (m == 5) || (m == 7) || (m == 8) || (m == 10) || (m == 12) ? 31 :
-           diasFebrero(a);
+int cantDias(int mes, int anio) {
+    return (mes == 2) ? diasFebrero(anio) : (mes == 4) || (mes == 6) || (mes == 9) || (mes == 11) ? 30 : 31;
 }
 
 int diasFebrero(int anio) {
     return anio % 4 == 0 && (anio % 100 != 0 || anio % 400 == 0) ? 29 : 28;
+    //calculo de año bisiesto
 }
 
 bool esMesValido(int mes) {
@@ -54,6 +50,6 @@ bool esAnioValido(int anio) {
 }
 
 bool mensajeDatoInvalido() {
-    cout << "Algun dato ingresado no es valido";
+    cout << "Algun dato ingresado no es valido. ";
     return false;
 }
